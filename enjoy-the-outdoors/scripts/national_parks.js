75cda5b77646fbd645442293eb8489799fa66144
bor564 <borronald5@gmail.com>
2023-11-17 08:53:19 -0500

    updates

--- enjoy-the-outdoors/scripts/national_parks.js
+++ enjoy-the-outdoors/scripts/national_parks.js
@@ -1,63 +1,87 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Get references to HTML elements
-  const locationSelect = document.getElementById("locationSelect");
-  const parkTypeSelect = document.getElementById("parkTypeSelect");
-  const parksTable = document.getElementById("parksTable");
-  const searchButton = document.getElementById("searchButton");
-
-  // Populate location dropdown
-  loadDropdown(locationSelect, locationsArray);
-
-  // Populate park type dropdown
-  loadDropdown(parkTypeSelect, parkTypesArray);
-
-  // Load parks into the table
-  loadParksTable(nationalParksArray);
-
-  //Event listeners for dropdown changes
-  searchButton.addEventListener("click", function () {
-    filterParks();
+    const locationSelect = document.getElementById("locationSelect");
+    const parkTypeSelect = document.getElementById("parkTypeSelect");
+    const searchButton = document.getElementById("searchButton");
+  
+    loadDropdown(locationSelect, locationsArray);
+    loadDropdown(parkTypeSelect, parkTypesArray);
+    loadParksTable(nationalParksArray);
+  
+    searchButton.addEventListener("click", function () {
+      filterParks();
+    });
   });
-});
-
-// Function to load dropdown with data
-function loadDropdown(select, data) {
-  data.forEach((item) => {
-    const option = document.createElement("option");
-    option.value = item;
-    option.textContent = item;
-    select.appendChild(option);
-  });
-}
-
-// Function to load parks into the table
-function loadParksTable(parks) {
-  const tableBody = document.getElementById("parksTableBody");
-  tableBody.innerHTML = "";
-
-  parks.forEach((park) => {
-    const row = tableBody.insertRow();
-    const cellName = row.insertCell(0);
-    const cellLocation = row.insertCell(1);
-    const cellType = row.insertCell(2);
-
-    cellName.textContent = park.Name;
-    cellLocation.textContent = park.State;
-    cellType.textContent = park.LocationName;
-  });
-}
-
-// Function to filter parks based on dropdown selections
-function filterParks() {
-  const locationFilter = document.getElementById("locationSelect").value;
-  const parkTypeFilter = document.getElementById("parkTypeSelect").value;
-
-  const filteredParks = nationalParksArray.filter((park) => {
-    const locationMatch = locationFilter === "All" || park.State === locationFilter;
-    const typeMatch = parkTypeFilter === "All" || park.LocationName.includes(parkTypeFilter);
-
-    return locationMatch && typeMatch;
-  });
-
-  loadParksTable(filteredParks);
-}
+  
+  function loadDropdown(select, data) {
+    const valueNone = new Option("None");
+    select.appendChild(valueNone);
+  
+    for (const item of data) {
+      const option = document.createElement("option");
+      option.value = item;
+      option.textContent = item;
+      select.appendChild(option);
+    }
+  }
+  
+  function loadParksTable(parks) {
+    const table = document.getElementById("parksTable");
+    table.innerHTML = "";
+  
+    const headerRow = table.insertRow();
+    const headers = ["Name", "Location", "Phone", "Fax", "Address", "City", "State", "Zip Code"];
+    for (const headerText of headers) {
+      const header = document.createElement("th");
+      header.textContent = headerText;
+      headerRow.appendChild(header);
+    }
+  
+    for (const park of parks) {
+      const row = table.insertRow();
+      const cellName = row.insertCell(0);
+      const cellLocation = row.insertCell(1);
+      const cellPhone = row.insertCell(2);
+      const cellFax = row.insertCell(3);
+      const cellAddress = row.insertCell(4);
+      const cellCity = row.insertCell(5);
+      const cellState = row.insertCell(6);
+      const cellZipCode = row.insertCell(7);
+  
+      cellName.textContent = park.LocationName;
+      cellLocation.textContent = park.State;
+      cellPhone.textContent = park.Phone ? park.Phone : '';
+      cellFax.textContent = park.Fax ? park.Fax : '';
+      cellAddress.textContent = park.Address ? park.Address : '';
+      cellCity.textContent = park.City ? park.City : '';
+      cellState.textContent = park.State ? park.State : '';
+      cellZipCode.textContent = park.ZipCode ? park.ZipCode : '';
+    }
+  }
+  
+  function filterParks() {
+    const locationFilter = document.getElementById("locationSelect").value;
+    const parkTypeFilter = document.getElementById("parkTypeSelect").value;
+  
+    let filteredParks;
+  
+    if (locationFilter === "None" && parkTypeFilter !== "None") {
+      filteredParks = nationalParksArray.filter(function (park) {
+        return park.LocationName.includes(parkTypeFilter);
+      });
+    } else if (locationFilter !== "None" && parkTypeFilter === "None") {
+      filteredParks = nationalParksArray.filter(function (park) {
+        return park.State === locationFilter;
+      });
+    } else if (locationFilter !== "None" && parkTypeFilter !== "None") {
+      filteredParks = nationalParksArray.filter(function (park) {
+        return (
+          park.State === locationFilter &&
+          park.LocationName.includes(parkTypeFilter)
+        );
+      });
+    } else {
+      filteredParks = nationalParksArray;
+    }
+  
+    loadParksTable(filteredParks);
+  }
