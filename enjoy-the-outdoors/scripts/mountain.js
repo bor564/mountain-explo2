10d9e69617fa522f9dc44e5213b5b8edf7f1a57c
bor564 <borronald5@gmail.com>
2023-11-21 15:51:38 -0500

    update

--- enjoy-the-outdoors/scripts/mountain.js
+++ enjoy-the-outdoors/scripts/mountain.js
@@ -1,91 +1,51 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Get references to HTML elements
-    const locationSelect = document.getElementById('locationSelect');
-    const parkTypeSelect = document.getElementById('parkTypeSelect');
-    const parksTable = document.getElementById('parksTable');
-    const mountainSelect = document.getElementById('mountainSelect');
-    const mountainInfoDiv = document.getElementById('mountainInfo');
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to HTML elements
+  
+  const mountainSelect = document.getElementById("mountainSelect");
+  const mountainInfoDiv = document.getElementById("mountainInfoDiv");
 
-window.onload = intit;
+  window.onload = intit;
 
-function intit(){
-    loadDropdown()
-}
-function moutainDataStorage(){
-    rightDivcontainer.innerHTML = "";
+  function intit() {
+    loadDropdown();
+  }
+  
 
-    leftDivContainer.innerHTML="";
+  function displayMountainInfo() {
+    const selectedMountain = mountainSelect.value;
+    const mountain = mountainsArray.find((m) => m.name === selectedMountain);
+
+    if (mountain) {
+      const mountainInfo = `
+              <h2>${mountain.name}</h2>
+              <p>Description: ${mountain.desc}</p>
+              <p>Elevation: ${mountain.elevation} feet</p>
+              <p>Effort: ${mountain.effort}</p>
+              <!-- Add more information as needed -->
+  
+              <!-- Optional: Display mountain image -->
+              <img src="images/${mountain.img}" alt="${mountain.name}">
+          `;
+
+      mountainInfoDiv.innerHTML = mountainInfo;
+    } else {
+      mountainInfoDiv.innerHTML = "<p>No information available for the selected mountain.</p>";
+    }
+  }
+
+  function loadDropdown() {
     for (const mountain of mountainsArray) {
-        if (mountain.name == mountainSelect.value) {  // Fix: Change mountainSelectList to mountainSelect
-            const mountainTitle = document.createElement("h2");
-            mountainTitle.innerText = mountain.name;
-    
-            const mountainSubData = document.createElement("h5");
-            mountainSubData.innerText = `Effort Level: ${mountain.effort} Elevation: ${mountain.elevation} ft.`;
-            mountainSubData.className = "text-white-50";
-    
-            const horizontalRule = document.createElement("hr");
-    
-            const mountainDescription = document.createElement("p");
-            mountainDescription.innerHTML = mountain.desc;
-    
-            // Appending the elements
-            rightDivContainer.appendChild(mountainTitle);
-            rightDivContainer.appendChild(mountainSubData);
-            rightDivContainer.appendChild(horizontalRule);
-            rightDivContainer.appendChild(mountainDescription);
-    
-            const mountainImg = document.createElement("img")
-            mountainImg.src = "imgs/" + mountain.img;
-            mountainImg.className = "w-100 rounded"
-            leftDivContainer.appendChild(mountainImg);
-        }
+      let option = new Option(mountain.name);
+      mountainSelect.appendChild(option);
     }
-    
-}  
+  }
+  
 
-    function loadDropdown(){
-        for (const mountain of mountainsArray) {
-            let option = new Option(mountain.name)
-            mountainSelect.appendChild(option);
-        }
-    }
-    // Populate location dropdown
+  // Event listeners for dropdown changes
 
-    // Event listeners for dropdown changes
-    locationSelect.addEventListener('change', function () {
-        filterParks();
-    });
-
-    parkTypeSelect.addEventListener('change', function () {
-        filterParks();
-    });
-
-    mountainSelect.addEventListener('change', function () {
-        displayMountainInfo();
-    });
+  mountainSelect.addEventListener("change", function () {
+    displayMountainInfo();
+  });
 });
 
 // Function to display information about the selected mountain
-function displayMountainInfo() {
-    const selectedMountain = mountainSelect.value;
-    const mountain = mountainsArray.find(m => m.name === selectedMountain);
-
-    if (mountain) {
-        const mountainInfo = `
-            <h2>${mountain.name}</h2>
-            <p>Description: ${mountain.desc}</p>
-            <p>Elevation: ${mountain.elevation} feet</p>
-            <p>Effort: ${mountain.effort}</p>
-            <!-- Add more information as needed -->
-
-            <!-- Optional: Display mountain image -->
-            <img src="images/${mountain.img}" alt="${mountain.name}">
-        `;
-
-        mountainInfoDiv.innerHTML = mountainInfo;
-    } else {
-        mountainInfoDiv.innerHTML = '<p>No information available for the selected mountain.</p>';
-    }
-}
-
